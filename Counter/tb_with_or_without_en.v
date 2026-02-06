08602f069518fc9ceabdcefddcd3205fc39376a8
Vinayaktodakar <120154156+Vinayaktodakar@users.noreply.github.com>
2026-02-06 22:42:21 +0530

    Add testbench for up_down counter module

---
+++ Counter/tb_with_or_without_en.v
@@ -0,0 +1,32 @@
+module tb;
+  reg clk,rst,up_dn,en;
+  wire [4:0] count;
+  
+ up_dwn #(.cnt_width(5)) uut( .clk(clk), .rst(rst), .up_dn(up_dn), .count(count));
+ //up_dwn #(.cnt_width(5)) uut( .clk(clk), .rst(rst), .up_dn(up_dn), .count(count), .en(en));
+  always #5 clk= ~clk;
+  initial begin
+	  clk=0;
+	  rst=1;
+	  up_dn=0;
+	  #15
+	  en=0;
+	  rst=0;
+	  #5 en=1;
+	  up_dn=1;
+	  #170
+	  en=0;
+	  up_dn=0;
+	  #60 en=1;
+	  rst=1;
+	  #10
+	  rst=0;
+	  up_dn=1;
+	  #100
+	   up_dn=0;
+	  #100
+	  $finish;
+  end
+  initial $monitor($time,"   up or down : %0d, | rst :%0d | count : %0d | ",up_dn,rst,count);
+  //initial $monitor($time,"   up or down : %0d, | rst :%0d | en : %0d | count : %0d | ",up_dn,rst,en,count);
+endmodule
